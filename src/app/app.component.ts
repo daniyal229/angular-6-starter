46d8b5e193a19ca8b86ddf052c4ca5a4d0adec80
Syed Daniyal Shah <daniyal.shah@softrove.com>
2018-06-20 05:09:49 -0700

    first component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { UserComponent } from './user.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  parent_title = this.constructor.name;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from "./user.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, UserComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,UserComponent]
 })
 export class AppModule { }
